279ac1dc27bfaf319198642b8e2d3ef4b58df237
oussama sikaddour <34127545+oussamaSikaddour@users.noreply.github.com>
2024-01-11 19:46:38 +0100

    an other update

--- js/common.js
+++ js/common.js
@@ -100,13 +100,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////                                                                                 toolTip
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const buttonsWithTooltip = document.querySelectorAll(".hasToolTip");
+const showToolTip = (toolTip, parent) => {
+  toolTip.classList.add("show");
+  parent.setAttribute("aria-expanded", true);
+  toolTip.setAttribute("aria-hidden", false);
+};
 
+const hideToolTip = (toolTip, parent) => {
+  toolTip.classList.remove("show");
+  parent.setAttribute("aria-expanded", false);
+  toolTip.setAttribute("aria-hidden", true);
+};
 
-
-
-
-
-
+buttonsWithTooltip.forEach(button => {
+  const toolTip = button.querySelector(".toolTip");
+  if (toolTip) {
+    button.addEventListener('mouseover', () => {
+      showToolTip(toolTip, button);
+    });
+    button.addEventListener('mouseout', () => {
+      hideToolTip(toolTip, button);
+    });
+  }
+});
 
 
 
